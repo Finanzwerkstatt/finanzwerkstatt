f0414b6acd71c75806b895f6b30402ab7a1d79b2
Finanzwerkstatt <kontakt@finanz-werkstatt.at>
2025-11-09 16:41:21 +0100

    fix vercel build config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: 'export',
-  distDir: 'out', // <-- sagt Next.js, es soll in /out schreiben
-  turbopack: {
-    root: __dirname,
-  },
+  reactStrictMode: true,
 };
-
 export default nextConfig;
+
