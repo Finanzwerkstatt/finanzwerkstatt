a96733c16206a61d04f5a87ca027e0e4c31aef48
Finanzwerkstatt <kontakt@finanz-werkstatt.at>
2025-11-09 14:13:51 +0100

    Add GitHub Pages auto-deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out', // <-- sagt Next.js, es soll in /out schreiben
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
